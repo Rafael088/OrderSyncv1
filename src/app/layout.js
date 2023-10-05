44e2d36fe84aaa5866c626fc5a1a97bd106e021a
Rafael088 <jerafa088@gmail.com>
2023-10-04 20:21:46 -0500

    f

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Providers from './providers'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'OSync',
+  title: 'Osync',
   description: 'Sincroniza tus sabores y eleva tu servicios',
 }
 
